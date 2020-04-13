01e7ae60d7ae4706dad29fe000b9e5403bb22c27
Maciej Mleczko <maciej.mleczko.net@gmail.com>
2020-04-13 16:36:19 +0200

    added Lesson 2 with code example

---
+++ Lesson_2/script_02.py
@@ -0,0 +1,18 @@
+# My first python script in python training
+
+# Value assigment with '=' operator
+script_info = "Lesson 2/7"
+script_info += "\n[Functions, docstrings, arguments]\n"
+
+# Example function
+
+def division_variables(a,b):
+    result = a/b
+    return result
+
+var1 = 6
+var2 = 34
+
+example = division_variables(var1, var2)
+
+print(example)
