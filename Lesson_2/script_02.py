a5ed2229d94d422dbfe2130e7e2a7ecc97c851ef
Maciej Mleczko <maciej.mleczko.net@gmail.com>
2020-04-15 02:16:37 +0200

    added function with default argument

--- Lesson_2/script_02.py
+++ Lesson_2/script_02.py
@@ -1,18 +1,43 @@
-# My first python script in python training
+# My second python script in python training
 
-# Value assigment with '=' operator
 script_info = "Lesson 2/7"
 script_info += "\n[Functions, docstrings, arguments]\n"
 
-# Example function
+print(script_info,"\n")
 
-def division_variables(a,b):
-    result = a/b
-    return result
+# Example function 1
 
 var1 = 6
 var2 = 34
 
+def division_variables(a,b):
+    """Division two numbers (int, float) 'a' and 'b'
+    parameters: a<int, float>, b<int, float>"""
+    result = a/b
+    return result
+
 example = division_variables(var1, var2)
 
-print(example)
+help(division_variables)
+print(example,"\n")
+
+
+# Example function 2
+
+my_text = "ala ma kota"
+
+# function without return (void)
+def show_text(text):
+    print(text+" <-- this text is print by function\n")
+
+show_text(my_text)
+
+
+# Function with arguments and default arguments
+
+def say_hi(name="Johny"):
+    result = "Hi "+name+"!"
+    return result
+
+print(say_hi(),say_hi("Peter"), sep="\n")
+
